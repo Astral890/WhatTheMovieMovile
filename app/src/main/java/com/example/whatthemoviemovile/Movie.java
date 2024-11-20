2c12328f0e19e85710f9d2f4d375c83d9fd04419
Astral890 <enac7125@outlook.com>
2024-11-19 18:05:17 -0600

    Chambea la api

--- app/src/main/java/com/example/whatthemoviemovile/Movie.java
+++ app/src/main/java/com/example/whatthemoviemovile/Movie.java
@@ -6,10 +6,46 @@ public class Movie {
     private String[] images;
     private int cont=0;
 
+    public Movie(){
+        this.images=new String[4];
+    }
+
     public Movie(int id, String title){
         this.id=id;
         this.title=title;
         images=new String[4];
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String[] getImages() {
+        return images;
+    }
+
+    public void setImages(String[] images) {
+        this.images = images;
+    }
+
+    public int getCont() {
+        return cont;
+    }
+
+    public void setCont(int cont) {
+        this.cont = cont;
     }
 
     public void addImages(String url){
--- app/src/main/java/com/example/whatthemoviemovile/Scifi.java
+++ app/src/main/java/com/example/whatthemoviemovile/Scifi.java
@@ -1,8 +1,10 @@
 package com.example.whatthemoviemovile;
 
-import android.os.AsyncTask;
+import android.content.Context;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,26 +12,34 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
 
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Scifi extends AppCompatActivity {
+
+    private TextView tv;
+    private Context context;
+    private List<Movie> movies=new ArrayList<>();
+    private RequestQueue requestQueue;
+    private StringRequest stringRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_scifi);
+        init();
+        requestMovieData();
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.scifi), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -37,66 +47,56 @@ public class Scifi extends AppCompatActivity {
         });
 
         ImageView display=findViewById(R.id.imgDisplay);
-
-        GetData getData=new GetData();
-        getData.execute();
-        //lamo a la api
-
-        //Consigo la pelicula
-        //Movie movie=new Movie(ans.id,ans.title);
-
-        //Consigo las imagenes
-
-        //Las meto en la movie
-        //movie.addImages(img.backdrops[0].file_path);
-
-        //Cambio la imagen
         Glide.with(this).load("https://image.tmdb.org/t/p/original/3sh2UA2Q2l7fihgoj1LhFFPTlIM.jpg?api_key=ada074b6a5691631b70bfbcaf68ebad9").into(display);
     }
-    public class GetData extends AsyncTask<String, String, String>{
+    public void init(){
 
-        @Override
-        protected String doInBackground(String... strings) {
-            String current="";
-            try{
-                URL url;
-                HttpURLConnection urlConnection=null;
+        tv=findViewById(R.id.tv12);
+        context=Scifi.this;
+    }
+    public void requestMovieData(){
+        requestQueue= Volley.newRequestQueue(context);
+        stringRequest=new StringRequest("https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=878&api_key=ada074b6a5691631b70bfbcaf68ebad9", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
                 try {
-                    url=new URL("https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=878");
-                    urlConnection=(HttpURLConnection) url.openConnection();
-                    InputStream is=urlConnection.getInputStream();
-                    InputStreamReader isr=new InputStreamReader(is);
-
-                    int data=isr.read();
-                    while (data!=-1){
-                        current+=(char) data;
-                        data=isr.read();
-                    }
-                    return current;
-
-                } catch (MalformedURLException e) {
-                    throw new RuntimeException(e);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }finally {
-                    if(urlConnection != null){
-                        urlConnection.disconnect();
-                    }
+                    JSONObject jsonObject = new JSONObject(response.toString());
+                    JSONArray jsonArray=jsonObject.getJSONArray("results");
+                    fetchData(jsonArray);
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
-            } catch (RuntimeException e) {
-                throw new RuntimeException(e);
+
             }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                showToast("API call error");
+            }
+        });
+        requestQueue.add(stringRequest);
+    }
+
+    private void fetchData(JSONArray jsonArray){
+        try {
+            JSONObject movie=jsonArray.getJSONObject(0);
+            movies.add(new Movie(
+                    movie.getInt("id"),
+                    movie.getString("title")
+            ));
+            if(!movies.isEmpty()){
+                tv.setText(movies.get(0).getTitle());
+            }else{
+                tv.setText("xd");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            showToast("Mobile detail error");
         }
 
-        @Override
-        protected void onPostExecute(String s) {
-            try{
-                JSONObject jsonObject=new JSONObject(s);
-                JSONArray jsonArray=jsonObject.getJSONArray("results");
-                for( )
-            } catch (JSONException e) {
-                throw new RuntimeException(e);
-            }
-        }
+    }
+
+    public void showToast(String msg){
+        Toast.makeText(context,msg,Toast.LENGTH_SHORT).show();
     }
 }
