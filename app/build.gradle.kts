9fcbb41480ff3bcdfe14f6b5e4fb55e1eaa74550
Susana Gallegos <mandalagc@gmail.com>
2024-11-20 16:30:56 -0600

    cambie algo del grade, le importe algo chiquito y ya y pues estilo basico del inicio de sesion
    inicio de sesion borrador

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,6 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -36,10 +35,11 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation ("com.github.bumptech.glide:glide:4.16.0")
-    implementation("com.android.volley:volley:1.2.1")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
     implementation("com.squareup.picasso:picasso:2.8")
+    implementation("com.android.volley:volley:1.2.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("com.google.android.material:material:1.9.0")
 }
